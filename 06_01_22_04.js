22e1fb32fbfe40fb7cba505bf1aac6c319d6a53c
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-07 09:25:18 +0700

    new pj

---
+++ 06_01_22_04.js
@@ -0,0 +1,6 @@
+function run(){
+    let a = parseInt(document.getElementById("x").value);
+    let b = parseInt(document.getElementById("y").value);
+    s = a*b
+    alert("Diện tích của hình chữ nhật là " + s)
+}
